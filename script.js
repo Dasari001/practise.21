5857c09cfbbb49e9fdd5bd7b1c58b2f857793e20
Dasari001 <dasarisaibabu01@gmail.com>
2023-02-28 07:59:07 +0530

    finish

--- script.js
+++ script.js
@@ -6,22 +6,69 @@ let arr = [
   { id: 3, name: "karen", age: "19", marks: 35 },
 ];
 
+// function logStudent(student){
+//   if(student.marks>50){
+//     console.log(student);
+//    }
+// }
+
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
+  arr.map(function (student){
+    if(student.marks>50){
+     console.log(student);
+    }
+  });
 }
+
+// arr.map(logStudent);
 
 function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach(function(student){
+    if(student.marks>50)
+    console.log(student);
+  });
 }
 
+// arr.forEach(logStudent);
+
 function addData() {
-  //Write your code here, just console.log
+  var student = {id:4,name:"susan",age:"20",marks:45};
+  arr.push(student);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
-  //Write your code here, just console.log
+  for(var i = 0; i<arr.length; i++){
+    if(arr[i].marks<50){
+      arr.splice(i,1);
+    }
+  }
+  console.log(arr);
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  let arr1 = [
+    { id: 4, name: "Ankit", age: "28", marks: 91 },
+    { id: 5, name: "Rahul", age: "29", marks: 85 },
+    { id: 6, name: "Simran", age: "19", marks: 95 },
+  ];
+  
+  // var finalArr = [];
+  
+//   arr.forEach(function(item){
+//     finalArr.push(item)
+//   });
+//   arr1.forEach(function(item){
+//     finalArr.push(item);
+// });
+
+
+
+        //  OR
+
+        var finalArr = [...arr,...arr1];
+
+
+console.log(finalArr);
+
 }
